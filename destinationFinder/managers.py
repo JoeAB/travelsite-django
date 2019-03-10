a0126909675097a86513f5e1c164115dab26a627
Joe Bennett <jab6677@gmail.com>
2019-03-10 18:34:57 -0400

    Not a lot of progress, but I had to do a lot of looking around before I found some interesting APIs to use for this. Going to use restcountries.eu. I've added an API manager and some basic call methods. Really needs to work to be fleshed out, but will go from there and start setting up some of the actual web application next.

---
+++ destinationFinder/managers.py
@@ -0,0 +1,24 @@
+import requests
+
+class RestCountriesManager:
+
+	API_PATH = 'https://restcountries.eu/rest/v2'
+
+	def query(self, requestString):
+		response = requests.get(requestString)
+		if response.status_code == 200:
+			return response.json()
+		else:
+			return response.status_code
+
+	def searchByName(self, nameQuery):
+		searchQuery= self.API_PATH +'/name/'+nameQuery
+		return self.query(searchQuery)
+
+	def getByCountryCode(self, code):
+		searchQuery= self.API_PATH +'/alpha/'+ code
+		return self.query(searchQuery)
+
+	def getCountriesByRegion(self, region):
+		searchQuery= self.API_PATH +'/region/'+ region
+		return self.query(searchQuery)
