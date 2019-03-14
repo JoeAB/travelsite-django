9281df44e33b40fb9b4a0956f15f62c982631123
Joe Bennett <jab6677@gmail.com>
2019-03-14 17:39:52 -0400

    Added a simple model class for blog posts.

--- destinationFinder/models.py
+++ destinationFinder/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class BlogPost(models.Model):
+	country_code = models.TextField(max_length=3)
+	title = models.TextField(max_length=150)
+	text = models.TextField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
