9b52eeb366a15a6ded33d61fbe807d189224a250
Joe Bennett <jab6677@gmail.com>
2019-03-16 07:08:24 -0400

    Added more to the models. Ideally, I'm going to get the application off of depending on the rest country manager to pull all data and use it for updates instead. Also want to add an API to get city data, so made classes to support all my future plans.

--- destinationFinder/models.py
+++ destinationFinder/models.py
@@ -2,9 +2,55 @@ from django.conf import settings
 from django.db import models
 
 
-# Create your models here.
+class Country(models.Model):
+	name = models.TextField()
+	country_code_alpha2 = models.CharField(max_length=3)
+	country_code_alpha3 = models.CharField(max_length=3)
+
+#split this out from country so we don't construct all this when showing our list page
+class CountryDetails(models.Model):
+	country = models.ForeignKey(Country, on_delete=models.CASCADE)
+	currencies = models.ManyToManyField('Currency', blank=True)
+	languages = models.ManyToManyField('Language', blank=True)
+	region = models.TextField(max_length=150)
+	subregion = models.TextField(max_length=150)
+	#integer should be fine unless we have a massive populaton boom or alien settlers
+	population = models.IntegerField()
+
+class Currency(models.Model):
+	countries = models.ManyToManyField('CountryDetails', blank=True)
+	name = models.TextField(max_length=150)
+	code = models.CharField(max_length=3)
+	symbol = models.CharField(max_length=250)
+
+class Language(models.Model):
+	countries = models.ManyToManyField('CountryDetails', blank=True)
+	name = models.TextField(max_length=150)
+	iso639_1 = models.CharField(max_length=2)
+	iso639_2 = models.CharField(max_length=2)
+
+class City(models.Model):
+	country = models.ForeignKey(Country, on_delete=models.CASCADE)
+	#Just in case there are long city names, like the full Thai name for Bangkok
+	name = models.TextField()
+	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6)
+	population = models.IntegerField()
+	utc_offset = models.DecimalField(max_digits=4, decimal_places=2)
+
+
 class BlogPost(models.Model):
-	country_code = models.TextField(max_length=3)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	topic_country = models.ForeignKey(Country, on_delete=models.CASCADE)
+	#topic city should be optional in the case of general country level postss
+	topic_city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True, null=True)
 	title = models.TextField(max_length=150)
 	text = models.TextField()
+	created_date = models.DateTimeField()
+
+class BlogComment(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+	text = models.TextField()
+
+
